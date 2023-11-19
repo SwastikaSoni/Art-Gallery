9e127f513ebcd75f71f177f795fe8d3478fcedc9
Srishti <srishti.09.2001@gmail.com>
2023-11-20 03:14:15 +0530

    added mongodb parts for get artworks routes

--- Backend/crud.js
+++ Backend/crud.js
@@ -1,37 +1,61 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
+// express setup
 const app = express();
 const PORT = 3000;
-
-// Middleware
 app.use(bodyParser.json());
 
+// mongo setup
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// for now, assuming dbname is 'mern-project' and collection name is 'artworks'
+const dbName = 'mern-project';
+const collectionName = 'artworks';
+const collection = client.db(dbName).collection(collectionName);
+
 // Dummy data for testing [have to replace this with MongoDb Database]
-let artworks = [
-  { id: 1, title: 'Madhubani ', artist: 'xyz' },
-  { id: 2, title: 'Mysore', artist: 'xyz' },
-  { id: 3, title: 'Kalam', artist: 'xyz' },
+// let artworks = [
+//   { id: 1, title: 'Madhubani ', artist: 'xyz' },
+//   { id: 2, title: 'Mysore', artist: 'xyz' },
+//   { id: 3, title: 'Kalam', artist: 'xyz' },
  
-];
+// ];
 
 // CRUD operations
 
 // Read all artworks
 app.get('/artworks', (req, res) => {
-  res.json(artworks);
+  collection.find({}).toArray(function(err, result) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    }
+    else {
+      console.log(result);
+      res.json(result);
+    }
+  });
 });
 
 // Read a specific artwork
 app.get('/artworks/:id', (req, res) => {
   const id = parseInt(req.params.id);
-  const artwork = artworks.find((a) => a.id === id);
+  
+  // find artwork by id, if not found return 404 error
+  collection.findOne({id: id}, function(err, result) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    } else if (!result) {
+      res.status(404).send('Artwork not found');
+    } else {
+      console.log(result);
+      res.json(result);
+    }
 
-  if (artwork) {
-    res.json(artwork);
-  } else {
-    res.status(404).json({ error: 'Artwork not found' });
-  }
+  });
 });
 
 // Create a new artwork
