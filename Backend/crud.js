2d0abc88dec85210fe761761290eb3669767086a
Srishti <srishti.09.2001@gmail.com>
2023-11-20 03:28:53 +0530

    Added MongoDb functionality for update and delete routes

--- Backend/crud.js
+++ Backend/crud.js
@@ -59,6 +59,8 @@ app.get('/artworks/:id', (req, res) => {
 });
 
 // Create a new artwork
+// TODO: add mongodb parts 
+// have to check schema first 
 app.post('/artworks', (req, res) => {
   const { title, artist } = req.body;
   const id = artworks.length + 1;
@@ -70,21 +72,49 @@ app.post('/artworks', (req, res) => {
 // Update an artwork
 app.put('/artworks/:id', (req, res) => {
   const id = parseInt(req.params.id);
-  const index = artworks.findIndex((a) => a.id === id);
 
-  if (index !== -1) {
-    artworks[index] = { id, ...req.body };
-    res.json(artworks[index]);
-  } else {
-    res.status(404).json({ error: 'Artwork not found' });
-  }
+  // find artwork by id in mongodb, if not found return 404 error
+  collection.findOne({id: id}, function(err, result) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    } else if (!result) {
+      res.status(404).send('Artwork not found');
+    } else {
+
+      // can add more fields here but need to check schema first
+      const { title, artist } = req.body;
+      const updatedArtwork = { id, title, artist };
+      // update artwork in mongodb
+      collection.updateOne({id: id}, {$set: updatedArtwork}, function(err, result) {
+        
+        if (err) {
+          console.error(err);
+          res.status(500).send('Internal Server Error');
+        } else {
+          console.log(result);
+          res.json(updatedArtwork);
+        }
+      });
+    }
+  });
 });
 
 // Delete an artwork
 app.delete('/artworks/:id', (req, res) => {
   const id = parseInt(req.params.id);
-  artworks = artworks.filter((a) => a.id !== id);
-  res.json({ message: 'Artwork deleted successfully' });
+  
+  collection.deleteOne({id: id}, function(err, result) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    } else if (!result) {
+      res.status(404).send('Artwork not found');
+    } else {
+      console.log(result);
+      res.json(result);
+    }
+  });
 });
 
 // Start the server
